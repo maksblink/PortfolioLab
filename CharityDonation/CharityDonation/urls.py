d0fa5afd91ed281ee929302578d3882c9a0510f9
maksblink <blinkmaks777@gmail.com>
2021-10-29 15:11:46 +0200

    templates done

--- CharityDonation/CharityDonation/urls.py
+++ CharityDonation/CharityDonation/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from main_app.views import HomeView
 
+from main_app.views import LandingPage, AddDonation, Login, Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^try$', HomeView.as_view(), name="home"),
-
+    re_path(r'^LandingPage$', LandingPage.as_view(), name="LandingPage"),
+    re_path(r'^AddDonation$', AddDonation.as_view(), name="AddDonation"),
+    re_path(r'^Login$', Login.as_view(), name="Login"),
+    re_path(r'^Register$', Register.as_view(), name="Register"),
 ]
--- CharityDonation/main_app/views.py
+++ CharityDonation/main_app/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.views import View
 
 
-class HomeView(View):
+class LandingPage(View):
     def get(self, request):
-        return render(request, "main_app/base.html")
+        return render(request, "main_app/index.html")
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, "main_app/form.html")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "main_app/login.html")
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, "main_app/register.html")
