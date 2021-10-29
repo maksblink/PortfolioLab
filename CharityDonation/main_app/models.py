4a780bbbf8460ef6be7e34b093d9cb0f263e525c
maksblink <blinkmaks777@gmail.com>
2021-10-29 22:11:25 +0200

    links

--- CharityDonation/main_app/models.py
+++ CharityDonation/main_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -16,3 +17,17 @@ class Institution(models.Model):
     description = models.CharField(max_length=128)
     type = models.CharField(choices=institution, max_length=23, default='fundacja')
     category = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, null=True, on_delete=models.SET_NULL)
+    address = models.CharField(max_length=128, help_text="ulica plus numer domu")
+    phone_number = models.CharField(max_length=12)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateTimeField(auto_now=True)
+    pick_up_time = models.DateTimeField(auto_now=True)
+    pick_up_comment = models.CharField(max_length=128)
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.SET_NULL)
--- CharityDonation/main_app/views.py
+++ CharityDonation/main_app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 
+from main_app.models import Donation, Institution
+
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "main_app/index.html")
+        donations = Donation.objects.all()
+        bags = 0
+        for donation in donations:
+            bags += donation.quantity
+        organizations = Institution.objects.count()
+        return render(request, "main_app/index.html", {'bags': bags, 'organizations': organizations})
 
 
 class AddDonation(View):
