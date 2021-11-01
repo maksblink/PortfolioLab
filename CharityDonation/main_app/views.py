c8587d1d76fe4bab7211283e1d265ec73e989ce4
maksblink <blinkmaks777@gmail.com>
2021-11-01 19:24:35 +0100

    login

--- CharityDonation/main_app/views.py
+++ CharityDonation/main_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate, login
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
 from django.views import View
 
 from main_app.models import Donation, Institution
+
+User = get_user_model()
 
 
 class LandingPage(View):
@@ -28,7 +32,32 @@ class Login(View):
     def get(self, request):
         return render(request, "main_app/login.html")
 
+    def post(self, request):
+        user = authenticate(username=request.POST.get('email'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/Register')
+
 
 class Register(View):
     def get(self, request):
         return render(request, "main_app/register.html")
+
+    def post(self, request):
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password != password2:
+            return render(request, "main_app/register.html", {'error': 'Passwords must be the same.'})
+        try:
+            new_user = User.objects.create_user(password=password,
+                                                username=request.POST.get('email'),
+                                                first_name=request.POST.get('name'),
+                                                last_name=request.POST.get('surname'),
+                                                email=request.POST.get('email'))
+        except IntegrityError:
+            return render(request, "main_app/register.html", {'error': 'This user already exists.'})
+        except ValueError:
+            return render(request, "main_app/register.html", {'error': 'You must complete all the fields.'})
+        return redirect('/Login')
